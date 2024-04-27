8273c462f19d1780ffe2e1c8e332879ffca3027e
lsy3232 <lhjoyh33@gmail.com>
2024-04-27 14:56:10 +0900

    Topdown 2D

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Talkmanager talkmanager;
+    public QuestManager questManager;
+    public Animator talkPanel;
+    public Image portraitImg;
+    public Animator portraitAnim;
+    public Sprite prePortrait;
+    public TypeEffect talk;
+    public TextMeshProUGUI questTalk;
+    public TextMeshProUGUI Name;
+    public GameObject menuSet;
+    public GameObject scanObject;
+    public GameObject player;
+    public bool isAction;
+    public int talkIndex;
+
+
+    void Start()
+    {
+        GameLoad();
+        questTalk.text = questManager.CheckQuest();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+            menuSet.SetActive(true);
+    }
+
+    public void Action(GameObject scanObj)
+    { 
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+        Name.text = objData.name;
+        
+        talkPanel.SetBool("isShow", isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        int questTalkIndex=0;
+        string talkData = "";
+        if(talk.isAnim){ 
+            talk.SetMsg("");
+            return;
+        }
+        else{
+        questTalkIndex = questManager.GetQuestTalkIndex(id);
+        talkData = talkmanager.GetTalk(id +questTalkIndex, talkIndex);
+        }
+
+        if (talkData == null) {
+            isAction = false;
+            talkIndex = 0;
+            questTalk.text = questManager.CheckQuest(id);
+            return;
+        }
+
+        if(isNpc){
+            talk.SetMsg(talkData.Split(':')[0]);
+            
+            portraitImg.sprite = talkmanager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1);
+            if ( prePortrait != portraitImg.sprite){
+                portraitAnim.SetTrigger("doEffect");
+                prePortrait = portraitImg.sprite;
+            }
+        }
+        else{
+            talk.SetMsg(talkData);
+
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
+    }
+
+    public void GameSave(){
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.SetInt("QuestId", questManager.questId);
+        PlayerPrefs.SetInt("QuestActionIndex", questManager.questActionIndex);
+        PlayerPrefs.Save();
+
+        menuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        if(!PlayerPrefs.HasKey("PlayerX")) return;
+        
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        int questId = PlayerPrefs.GetInt("QuestId");
+        int questActionIndex = PlayerPrefs.GetInt("QuestActionIndex");
+
+        player.transform.position = new Vector3(x,y,0);
+        questManager.questId = questId;
+        questManager.questActionIndex = questActionIndex;
+        questManager.ControlObject();
+    }
+
+    public void GameExit(){
+        Application.Quit();
+    }
+}
